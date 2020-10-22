3e6828ce8dd1f594670bcef384d9aba0219c5415
jennisun <jennifersun@Jennifers-MacBook-Air-2.local>
2020-10-22 15:15:17 -0400

    largest done

--- ArrayOps.java
+++ ArrayOps.java
@@ -3,10 +3,23 @@ public class ArrayOps {
   public static int sum(int[] arr) {
     int sum = 0;
 
-    for (int i = 0; i < arr.length; i ++) {
-      sum += arr[i];
+    if (arr.length != 0) {
+      for (int i = 0; i < arr.length; i ++) {
+        sum += arr[i];
+      }
+      return sum;
     }
-    return sum;
+
+    else return 0;
+  }
+
+
+  public static  int largest(int[]arr) {
+    int max = 0;
+    for (int i = 0; i < arr.length; i ++) {
+      if (arr[i] > max) max = arr[i];
+    }
+    return max;
   }
 
 
