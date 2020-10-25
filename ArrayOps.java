4c7b435166a0a77ad287af0f44b01f9978d158b2
jennisun <jennifersun@Jennifers-MacBook-Air-2.local>
2020-10-24 20:19:48 -0400

    hehe

--- ArrayOps.java
+++ ArrayOps.java
@@ -44,8 +44,25 @@ public class ArrayOps {
       }
     }
     return max;
-
    }
+
+
+   public static  int sum(int[][] arr) {
+     int[] sum = sumRows(arr);
+     int ans = 0;
+
+     for (int i = 0; i < sum.length; i ++) {
+       ans += sum[i];
+     }
+
+     return ans;
+   }
+
+
+
+
+
+
 
 
 
