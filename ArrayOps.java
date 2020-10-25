03b0974464ba299aef4c3fdbfc15a20eee5a80bf
jennisun <jennifersun@Jennifers-MacBook-Air-2.local>
2020-10-24 20:02:51 -0400

    yay

--- ArrayOps.java
+++ ArrayOps.java
@@ -14,13 +14,27 @@ public class ArrayOps {
   }
 
 
-  public static  int largest(int[]arr) {
+  public static int largest(int[]arr) {
     int max = 0;
     for (int i = 0; i < arr.length; i ++) {
       if (arr[i] > max) max = arr[i];
     }
     return max;
   }
+
+
+  public static int[] sumRows(int[][] matrix) {
+    int [] sum = new int [matrix.length];
+
+    for (int i = 0; i < matrix.length; i ++) {
+      for (int h = 0; h < matrix[i].length; h ++) {
+        sum[i] += matrix[i][h];
+      }
+    }
+    return sum;
+
+  }
+
 
 
 }
