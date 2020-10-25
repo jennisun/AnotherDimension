50f55daed8f695dc6098403ed74bb231a0785ba5
jennisun <jennifersun@Jennifers-MacBook-Air-2.local>
2020-10-25 17:14:30 -0400

    part 5 test cases

--- Tester.java
+++ Tester.java
@@ -7,11 +7,35 @@ public class Tester {
                 {  7, -2,  2,  1 },
                 { -5, -2,  2, -9 }
              };
-    int[]  B  =   {  1, 3, 5 };
+    int[]  B  =   {  1, 8, 5 };
 
-    System.out.println( ArrayOps.sum(B));
+    int[][] C = { {  1,  2, 3, 4 },
+   {  2, 3,  4,  1 },
+   { 3, 4,  1, 2 } };  //this is rowMagic but NOT colMagic
+
+int[][] D = { {  1,  1, 1 },
+   {  2, 2, 2 },
+   { 3,  3, 3 } };  //this is colMagic but NOT rowMagic
+
+int[][] E = { {  2,  2, 2 },
+   {  2, 2, 2 } };  //this is both colMagic AND rowMagic
+
+    System.out.println( ArrayOps.isRowMagic(C));
+    System.out.println( ArrayOps.isColMagic(C));
+
+    System.out.println( ArrayOps.isRowMagic(D));
+    System.out.println( ArrayOps.isColMagic(D));
+
+    System.out.println( ArrayOps.isRowMagic(E));
+    System.out.println( ArrayOps.isColMagic(E));
+
 
   }
+
+
+
+
+
 
 
 }
